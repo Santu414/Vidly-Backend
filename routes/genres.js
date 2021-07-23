05dd744ec572d3607e17a502ea54c3b99198d4d4
Santosh Reddy <snagankera@gmail.com>
2021-07-23 23:53:38 +0530

    Implemented Role Based Authentication

--- routes/genres.js
+++ routes/genres.js
@@ -2,6 +2,7 @@ const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 
 const { Genre, validateGenre } = require("../models/genre");
 
@@ -35,7 +36,7 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre)
