e33f9fca627aa3056c07576960181a6da8a189f9
Santosh Reddy <snagankera@gmail.com>
2021-07-23 19:42:48 +0530

    Authenticating the user and Testing the Authentication

--- routes/auth.js
+++ routes/auth.js
@@ -5,22 +5,31 @@ const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
-const { User, validate } = require("../models/user");
+const { User } = require("../models/user");
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User Already Reqistered... ");
+  if (!user) return res.status(400).send("Invalid User or Password ");
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid User or Password ");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
 
-  await user.save();
-
-  res.send(_.pick(user, ["_id", "name", "email"]));
+  res.send(true);
 });
+
+function validate(req) {
+  const schema = {
+    email: Joi.string().min(3).max(255).required().email(),
+    password: Joi.string().min(3).max(1024).required(),
+  };
+
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
