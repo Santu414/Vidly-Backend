4f01b6df97876107be895e428d744ba0725ce167
Santosh Reddy <snagankera@gmail.com>
2021-07-23 17:00:58 +0530

    Created the new user

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 1024,
+    },
+  })
+);
+
+function validateGenre(user) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(3).max(255).required().email(),
+    password: Joi.string().min(3).max(1024).required(),
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validateUser = validateUser;
